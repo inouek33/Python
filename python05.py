cb476abe79cfa4357c6c970e1a75a1cf02d2f03a
井上恵太 <keita.inoue06@gmail.com>
2016-02-01 19:27:37 +0900

    // 2016.02.01 Python break文とcontinue文、breakを使用したソースコード

---
+++ python05.py
@@ -0,0 +1,8 @@
+# break文とcontinue文、ループのelse句を使用した複合
+for n in range(2, 10):
+	for x in range(2, n):
+		if n % x == 0:
+			print n, 'equals', x, '*', n/x
+			break;
+    else :
+        print n, 'is a prime number' 
